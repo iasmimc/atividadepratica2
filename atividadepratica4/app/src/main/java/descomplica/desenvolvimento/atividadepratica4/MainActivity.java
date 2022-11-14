099a4f686d32fb86a6f2335327facc7f0650d5ee
Iasmim <iasmim.capra@gmail.com>
2022-11-14 11:54:04 +0000

    Aula Pratica Integradora de Desenvolvimento de APP- atividade 3.

---
+++ atividadepratica4/app/src/main/java/descomplica/desenvolvimento/atividadepratica4/MainActivity.java
@@ -0,0 +1,48 @@
+package descomplica.desenvolvimento.atividadepratica4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View v) {
+        if(player == null) {
+            player = MediaPlayer.create(this,R.raw.music);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {stopPlayer();}
+            });
+        }
+        player.start();
+    }
+
+    public void pause(View v) {
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    public void stop(View v) {stopPlayer();}
+
+    private void stopPlayer() {
+        if(player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this,"Media Player Liberado",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
